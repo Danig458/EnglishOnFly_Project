b87d693f05d29c803596336747c379dbde2d076c
Danig458 <danielagonzalez200416@gmail.com>
2025-03-13 13:14:56 -0500

    subido

---
+++ INGLES_PLATAFORMA/INGLES_PLATAFORMA/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace INGLES_PLATAFORMA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.BackColor = ColorTranslator.FromHtml("#0097b2");
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+           
+
+
+        }
+
+        private void txtContraseña_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void txtUsuario_Enter(object sender, EventArgs e)
+        {
+            if (txtUsuario.Text == "")
+            {
+                txtUsuario.Text = "USUARIO";
+                txtUsuario.ForeColor = Color.LightGray;
+            }
+        }
+
+        private void txtUsuario_Leave(object sender, EventArgs e)
+        {
+            if (txtUsuario.Text == "")
+            {
+                txtUsuario.Text = "N° DOCUMENTO";
+                txtUsuario.ForeColor = Color.MintCream;
+            }
+        }
+    }
+}
